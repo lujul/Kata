972c2a7ba6d508286e1c5054c6e33547db4eb187
julien <julien3b@gmail.com>
2016-09-27 14:44:24 +0200

    Implement Calculator Class with an add method and First functionality

---
+++ app/src/main/java/com/hbdriod/kata/Calculator.java
@@ -0,0 +1,24 @@
+package com.hbdriod.kata;
+
+/**
+ * Created by Human Booster on 27/09/2016.
+ */
+
+public class Calculator {
+
+
+    public Calculator() {
+    }
+
+    //Method who makes a sum of an input String value 
+    public int add(String numbers) {
+        if (numbers == "") {
+            return 0;
+        } else {
+            return -1;
+        }
+
+    }
+
+
+}
