a471be2e07a45067333816c46d3673ea81a7a005
julien <julien3b@gmail.com>
2016-09-27 13:18:30 +0200

    design_version_0

--- app/src/main/java/com/hbdriod/kata/MainActivity.java
+++ app/src/main/java/com/hbdriod/kata/MainActivity.java
@@ -1,7 +1,12 @@
 package com.hbdriod.kata;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setBackgroundColor(Color.GRAY);
+        textView.setTextColor(Color.GREEN);
+        final EditText editText = (EditText) findViewById(R.id.edit_text);
+        Button button = (Button) findViewById(R.id.button);
+
     }
 }
