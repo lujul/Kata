31ae24519100125d0b22bdb2e7e6ae59b76e40ae
julien <julien3b@gmail.com>
2016-09-27 13:34:29 +0200

    lib log deleted

--- app/src/main/java/com/hbdriod/kata/MainActivity.java
+++ app/src/main/java/com/hbdriod/kata/MainActivity.java
@@ -3,7 +3,6 @@ package com.hbdriod.kata;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
